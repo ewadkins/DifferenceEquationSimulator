ccf71c00403d261befb0627ab555ea8206b74043
ewadkins <ewadkins83@gmail.com>
2015-02-16 14:32:13 -0500

    Initial Commit

---
+++ Main/__init__.py
@@ -0,0 +1,52 @@
+    
+class differenceEquation:
+    def __init__(self, inputCoeffs, outputCoeffs):
+        self.inputCoeffs = inputCoeffs
+        self.outputCoeffs = outputCoeffs
+        self.oldInputs = [0 for i in range(len(inputCoeffs)-1)]
+        self.oldOutputs = [0 for i in range(len(outputCoeffs))]
+    
+    def step(self, inp):
+        result = 0
+        
+        #print
+        #print "Stepping with input = %i" % inp
+        #print "Old Inputs: %r" % self.oldInputs
+        #print "Old Outputs: %r" % self.oldOutputs
+        
+        for i in range(len(self.inputCoeffs)):
+            if(i == 0):
+                result = result + self.inputCoeffs[i] * inp
+            else:
+                result = result + self.inputCoeffs[i] * self.oldInputs[i-1]
+                
+        for i in range(len(self.outputCoeffs)):
+            result = result + self.outputCoeffs[i] * self.oldOutputs[i]
+        
+        if(len(self.oldInputs) > 0):
+            temp = [0 for i in range(len(self.oldInputs))]
+            temp[0] = inp
+            for i in range(1, len(self.oldInputs)):
+                temp[i] = self.oldInputs[i-1]
+            self.oldInputs = temp
+        
+        if(len(self.oldOutputs) > 0):
+            temp = [0 for i in range(len(self.oldOutputs))]
+            temp[0] = result
+            for i in range(1, len(self.oldOutputs)):
+                temp[i] = self.oldOutputs[i-1]
+            self.oldOutputs = temp
+        
+        return result
+        
+dif = differenceEquation([0, 0, 21], [0, -21])
+#print "Input Coeffs: %r" % dif.inputCoeffs
+#print "Output Coeffs: %r" % dif.outputCoeffs
+print dif.step(1)
+print dif.step(0)
+print dif.step(0)
+print dif.step(0)
+print dif.step(0)
+print dif.step(0)
+print dif.step(0)
+print dif.step(0)
